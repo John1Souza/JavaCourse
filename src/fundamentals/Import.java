13d3ab7ab6966d75436218ac825358cf418b26cb
John1Souza <109979241+John1Souza@users.noreply.github.com>
2023-04-07 11:35:35 -0300

    String type

--- src/fundamentals/Import.java
+++ src/fundamentals/Import.java
@@ -1,5 +1,15 @@
 package fundamentals;
 
+import java.util.Date;
+
 public class Import {
-    
+    public static void main(String[] args) {
+        String s = "Good Day!";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+        // JButton button = new JButton();
+    }
 }
