2d04fa95ae7349c1d0e2cef7454c4a1389d47113
John1Souza <109979241+John1Souza@users.noreply.github.com>
2023-04-07 20:35:01 -0300

    String type informations

--- src/fundamentals/StringType.java
+++ src/fundamentals/StringType.java
@@ -1,5 +1,42 @@
 package fundamentals;
 
 public class StringType {
+    public static void main(String[] args) {
+        System.out.println("Hello people!".charAt(1));
 
+        String s = "Good afternoon";
+        // s = s.toUpperCase();
+
+        System.out.println(s.concat("!!!"));
+        System.out.println(s + "!!!");
+        System.out.println(s.toLowerCase().startsWith("good"));
+        System.out.println(s.toLowerCase().endsWith("afternoon"));
+        System.out.println(s.length());
+        System.out.println(s.toLowerCase().equals("good afternoon"));
+        System.out.println(s.equalsIgnoreCase("good afternoon"));
+
+        var name = "Peter";
+        var lastName = "Santos";
+        var age = 33;
+        var wage = 12345.987;
+
+        System.out.println("Name: "
+                + name + "\nLast name: "
+                + lastName + "\nage: "
+                + age + "\nwage: "
+                + wage);
+
+        System.out.printf("Name: %S %S", name, lastName);
+        System.out.printf("\nThe Sr. %s %s have %d year old, and won $%.2f. dollars/mont",
+                name,
+                lastName,
+                age,
+                wage);
+        String phrase = String.format("\nThe Sr. %s %s have %d year old, and won $%.2f. dollars/mont",
+                name,
+                lastName,
+                age,
+                wage);
+        System.out.println(phrase);
+    }
 }
