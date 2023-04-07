ab519c02cb76a5b8976085718ef785eea21b0104
John1Souza <109979241+John1Souza@users.noreply.github.com>
2023-04-07 10:17:07 -0300

    dot notation

--- src/fundamentals/dotNotation.java
+++ src/fundamentals/dotNotation.java
@@ -1,4 +1,26 @@
 package fundamentals;
 
 public class dotNotation {
+    public static void main(String[] args) {
+
+        String s = "Good day! X";
+        s = s.replace("X", "Sr");
+        s = s.toUpperCase();
+        s = s.concat("!!!");
+
+
+
+        System.out.println(s);
+        System.out.println("John".toUpperCase());
+
+        String y = "Bom dia X"
+                .replace("X", "John")
+                .toUpperCase()
+                .concat("!!!");
+        System.out.println(y);
+
+        // Primitive types doesn't have "." operator
+        int a = 3;
+
+    }
 }
