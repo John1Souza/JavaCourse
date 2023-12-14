ecbe8013de65fee5a517fa6ed0753c34a3572ec4
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2023-12-14 09:12:59 -0300

    lamba concepts with for each

--- src/Lambdas/Foreach.java
+++ src/Lambdas/Foreach.java
@@ -6,5 +6,25 @@ import java.util.List;
 public class Foreach {
     public static void main(String[] args) {
         List<String> approved = Arrays.asList("Ana", "Bia", "Gui", "Lia");
+
+        for(String name: approved){
+            System.out.println(name);
+        }
+
+        System.out.println("\nLambda #01...");
+        approved.forEach(name -> System.out.println(name + "!!!!"));
+
+        System.out.println("\nMethod Reference...");
+        approved.forEach(System.out::println);
+
+        System.out.println("\nLambda #02...");
+        approved.forEach(name -> myPrint(name + "!!!!"));
+
+        System.out.println("\nMethod Reference...");
+        approved.forEach(Foreach::myPrint);
+    }
+
+    static void myPrint(String name){
+        System.out.println("Hi! My name is " + name);
     }
 }
