a550f95e318ee53f181b217fa427c218d48dd9ce
John1Souza <johnatas2014@hotmail.com>
2024-01-03 20:55:25 -0300

    [UPDATE] introduction to swing with observer concepts

---
+++ src/swing/Observer.java
@@ -0,0 +1,38 @@
+package swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class Observer {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Observer");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(600, 200);
+        window.setLayout(new FlowLayout());
+        window.setLocationRelativeTo(null); // Centralizar na tela do PC
+
+        JButton button = new JButton("Click!");
+        window.add(button);
+
+        button.addActionListener(event -> {
+            System.out.println("An event occurred!");
+        });
+
+        window.addFocusListener(new FocusListener() {
+
+            public void focusGained(FocusEvent e) {
+
+            }
+
+
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+        window.setVisible(true);
+    }
+}
